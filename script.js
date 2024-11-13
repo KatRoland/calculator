9cdf3e0ab3ebcc04bb852a4e9102e4e54b76ba74
KatRoland <rolikaac@gmail.com>
2024-11-13 09:37:34 +0100

    full functionality

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 document.getElementById("calcbtn").addEventListener("click", () => {
-    document.getElementById("calculator").classList.toggle("visible");
+    document.getElementById("calculator").classList.add("visible");
+})
+
+let currentoperation = ""
+let base = ""
+let current = ""
+const currentnumberelement = document.getElementById("currentnumber")
+const prevnumberelement = document.getElementById("prevnumber")
+
+
+document.getElementById("closebtn").addEventListener("click", () => {
+    document.getElementById("calculator").classList.remove("visible");
+    currentoperation = ""
+    base = ""
+    current = ""
+    currentnumberelement.textContent = 0;
+    prevnumberelement.textContent = "";
 })
 
 const buttons = document.querySelectorAll(".btn")
@@ -10,24 +26,73 @@ buttons.forEach( (elem) => {
 } )
 
 //TODO: keydown handling
-window.addEventListener("onKeyDown", (key) => {
-    e.preventDefaults
-    press(key)
+window.addEventListener("keydown", (e) => {
+    press(e.key)
 })
 
-let base = ""
-let current = ""
+
+
 
 function press(btn) {
+    
     console.log(btn)
-    if(!isNaN(btn)) {
-        current += btn
-        console.log("isnan")
-        document.getElementById("currentnumber").textContent = current
-    } else if(btn == "clear"){
-        base = ""
-        current = ""
-        document.getElementById("currentnumber").textContent = 0
+    if (!isNaN(btn)) {
+        current += btn;
+        currentnumberelement.textContent = current;
+    } else {
+        switch (btn) {
+            case "Delete":
+                base = current = currentoperation = "";
+                currentnumberelement.textContent = 0;
+                prevnumberelement.textContent = `${base} ${currentoperation}`;
+                break;
+            case "Backspace":
+                current = "";
+                currentnumberelement.textContent = 0;
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                currentoperation = btn;
+                base = current;
+                current = "";
+                currentnumberelement.textContent = 0;
+                prevnumberelement.textContent = `${base} ${currentoperation}`;
+                break;
+            case "pm":
+                if(current.includes("-")) {
+                    current = current.replace("-", "");
+                } else {
+                    current = "-" + current;
+                }
+                currentnumberelement.textContent = current;
+                break;
+            case "square":
+                current = Math.pow(current, 2);
+                currentnumberelement.textContent = current;
+                break;
+            case "=":
+            case "Enter":
+                current = eval(base + currentoperation + current);
+                currentnumberelement.textContent = current;
+                base = "";
+                break;
+            case ".":
+            case ",":
+                if (!current.includes(".")) {
+                    current += ".";
+                    currentnumberelement.textContent = current;
+                }
+                break;
+            default: 
+                return;
+        }
+        
     }
+    document.getElementById(btn).classList.add("clicked");
+    setTimeout(() => {
+        document.getElementById(btn).classList.remove("clicked");
+    }, 300)
     console.log(current)
 }
